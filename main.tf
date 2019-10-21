d02c0b15955150812c2a5bd748eebb49fe8ef0e9
thanhbn <thanhbn87@gmail.com>
2019-10-21 10:40:04 +0700

    initial

---
+++ main.tf
@@ -0,0 +1,56 @@
+provider "aws" {
+  region  = "${var.aws_region}"
+  profile = "${var.aws_profile}"
+}
+
+terraform {
+  backend "s3" {}
+}
+
+data "terraform_remote_state" "vpc" {
+  backend = "s3"
+
+  config {
+    bucket         = "${var.tfstate_bucket}"
+    key            = "${var.tfstate_key_vpc}"
+    region         = "${var.tfstate_region}"
+    profile        = "${var.tfstate_profile}"
+    role_arn       = "${var.tfstate_arn}"
+  }
+}
+
+data "aws_security_group" "efs" {
+  tags   = "${merge(var.source_security_group_tags,map("Env", "${var.project_env}"))}"
+  vpc_id = ["${data.terraform_remote_state.vpc.vpc_id}"]
+}
+
+resource "null_resource" "azs" {
+  count    = "${length(local.subnets}"
+  triggers = {
+    az = "${element(data.aws_availability_zones.available.names,count.index)}"
+  }
+}
+
+locals {
+  zone_id = "${var.dns_private ? data.terraform_remote_state.vpc.private_zone_id : "" }"
+  subnets = "${data.terraform_remote_state.vpc.private_subnets}"
+  azs     = "${flatten(null_resource.azs.*.triggers.az)}"
+}
+
+module "efs" {
+  source     = "git::https://github.com/thanhbn87/terraform-aws-efs.git?ref=0.9.1"
+  namespace  = "${var.namespace}"
+  stage      = "${var.project_env}"
+  name       = "${var.name}"
+  attributes = ["efs"]
+
+  aws_region         = "${var.aws_region}"
+  vpc_id             = "${data.terraform_remote_state.vpc.vpc_id}"
+  subnets            = ["${local.subnets}"]
+  availability_zones = ["${local.azs}"]
+  security_group_id  = "${data.aws_security_group.efs.id}"
+
+  zone_id  = "${local.zone_id}"
+  dns_name = "${var.dns_name}"
+  tags     = "${merge(var.tags,map("Env", "${var.project_env}"))}"
+}
---
+++ vars.tf
@@ -0,0 +1,21 @@
+variable "project_name" { default = "Demo" }
+variable "project_env" { default = "Production" }
+variable "project_env_short" { default = "prd" }
+
+variable "aws_region" { default = "us-west-2" }
+variable "aws_profile" { default = "default" }
+
+variable "tags" {
+  default = {}
+}
+
+variable "tfstate_bucket" { default = "example-tfstate-bucket" }
+variable "tfstate_region" { default = "us-west-2" }
+variable "tfstate_profile" { default = "default" }
+variable "tfstate_arn" { default = "" }
+variable "tfstate_key_vpc" { default = "demo/vpc/terraform.tfstate" }
+
+variable "name" { default = "" }
+variable "namespace" { default = "" }
+variable "source_security_group_tags" { default = {} }
+variable "dns_private" { default = true }
